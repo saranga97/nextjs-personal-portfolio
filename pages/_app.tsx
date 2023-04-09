d140095b54ec69cd03890da976ab706c28833799
Saranga Nayanajith <saranga.karunasundara@gmail.com>
2023-04-09 23:41:21 +0530

    final

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,22 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 
+import { ChakraProvider } from "@chakra-ui/react";
+import AppContainer from "../components/AppContainer";
+import theme from "../config/theme";
+
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ChakraProvider>
+
+        <Component {...pageProps} />
+
+    </ChakraProvider>
+  );
 }
+
+{/* <ChakraProvider theme={theme}>
+      <AppContainer>
+        <Component {...pageProps} />
+      </AppContainer>
+    </ChakraProvider> */}
